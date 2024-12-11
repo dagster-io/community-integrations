a3524c6293a9fe207f459c48eae84e47bac689c1
danielgafni <danielgafni16@gmail.com>
2024-12-11 17:14:14 +0100

    add example (not working yet)

--- libraries/pipes/implementations/java/dagster-pipes-java/examples/local/definitions.py
+++ libraries/pipes/implementations/java/dagster-pipes-java/examples/local/definitions.py
@@ -1,13 +1,27 @@
 import dagster as dg
 
 
+from contextlib import contextmanager
+
+
 @dg.asset(
     # check_specs=[dg.AssetCheckSpec(name="orders_id_has_no_nulls", asset="count_adult_users")]
 )
 def count_adult_users(
     context: dg.AssetExecutionContext, pipes_subprocess_client: dg.PipesSubprocessClient
 ):
-    context.get_asset_provenance
+    @contextmanager
+    def capture_params(*args, **kwargs):
+        with pipes_subprocess_client.context_injector.__class__.inject_context(
+            pipes_subprocess_client.context_injector, *args, **kwargs
+        ) as params:
+            print(params)
+            with open(params["path"], "r") as f:
+                print(f.read())
+            yield params
+
+    pipes_subprocess_client.context_injector.inject_context = capture_params
+
     return pipes_subprocess_client.run(
         context=context,
         command=[
