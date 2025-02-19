bf2f5df280767b89770ec402db7633accca2ad98
Jasper Ginn <jasperginn@gmail.com>
2025-02-19 14:09:34 +0100

    chore: lint

--- libraries/dagster-iceberg/src/dagster_iceberg/_utils/__init__.py
+++ libraries/dagster-iceberg/src/dagster_iceberg/_utils/__init__.py
@@ -3,8 +3,8 @@ from dagster_iceberg._utils.partitions import (
     DagsterPartitionToDaftSqlPredicateMapper as DagsterPartitionToDaftSqlPredicateMapper,
 )
 from dagster_iceberg._utils.partitions import (
-    DagsterPartitionToPolarsSqlPredicateMapper as DagsterPartitionToPolarsSqlPredicateMapper,
+    DagsterPartitionToIcebergExpressionMapper as DagsterPartitionToIcebergExpressionMapper,
 )
 from dagster_iceberg._utils.partitions import (
-    DagsterPartitionToIcebergExpressionMapper as DagsterPartitionToIcebergExpressionMapper,
+    DagsterPartitionToPolarsSqlPredicateMapper as DagsterPartitionToPolarsSqlPredicateMapper,
 )
