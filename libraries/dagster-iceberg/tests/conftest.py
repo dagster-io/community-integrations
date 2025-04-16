b665e2e433a1382c661cbea32e5fc9472df39919
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-04-16 15:57:20 -0600

    Try a not temporary directory

--- libraries/dagster-iceberg/tests/conftest.py
+++ libraries/dagster-iceberg/tests/conftest.py
@@ -3,6 +3,7 @@ import random
 import shutil
 import subprocess
 from collections.abc import Iterator
+from pathlib import Path
 
 import psycopg2
 import pyarrow as pa
@@ -18,16 +19,12 @@ POSTGRES_DB = "test"
 POSTGRES_HOST = "localhost"
 POSTGRES_PORT = 5432
 
-WAREHOUSE_DIR = "warehouse"
-
 
 @pytest.fixture(scope="session", autouse=True)
 def compose(tmp_path_factory: pytest.TempPathFactory) -> Iterator[None]:
     # Determine the warehouse path temporary directory pytest will make:
     # https://github.com/pytest-dev/pytest/blob/b48e23d/src/_pytest/tmpdir.py#L67
-    warehouse_path = str(
-        tmp_path_factory.getbasetemp().joinpath(WAREHOUSE_DIR).resolve()
-    )
+    warehouse_path = str(Path("my-warehouse-dir").resolve())
 
     subprocess.run(
         ["docker", "compose", "up", "--build", "--wait"],
@@ -82,10 +79,11 @@ def warehouse_path(tmp_path_factory: pytest.TempPathFactory) -> str:
     # Determine the warehouse path temporary directory pytest will make:
     # https://github.com/pytest-dev/pytest/blob/b48e23d/src/_pytest/tmpdir.py#L67
     # TODO(deepyaman): Figure out why teardown isn't called in cloud CI.
-    if (old_dir := tmp_path_factory.getbasetemp().joinpath(WAREHOUSE_DIR)).exists():
+    if (old_dir := Path("my-warehouse-dir")).exists():
         shutil.rmtree(old_dir)
 
-    dir_ = tmp_path_factory.mktemp(WAREHOUSE_DIR, numbered=False)
+    dir_ = Path("my-warehouse-dir")
+    dir_.mkdir()
     yield str(dir_.resolve())
     shutil.rmtree(dir_)
 
