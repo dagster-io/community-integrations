45a97b98bb16824b67f0b73259f8222e34e1c4e3
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-04-17 15:59:00 -0600

    Revert "[dagster-iceberg] Copy Spark config to exec folder"
    This reverts commit f4e5f659dd082ed31a351163c19f91ab504c0613.

--- libraries/dagster-iceberg/tests/io_managers/test_spark_io_manager.py
+++ libraries/dagster-iceberg/tests/io_managers/test_spark_io_manager.py
@@ -8,6 +8,22 @@ from pyspark.sql.connect.dataframe import DataFrame
 
 from dagster_iceberg.io_manager.spark import SparkIcebergIOManager
 
+SPARK_CONFIG = {
+    "spark.sql.extensions": "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
+    "spark.sql.catalog.postgres": "org.apache.iceberg.spark.SparkCatalog",
+    "spark.sql.catalog.postgres.type": "jdbc",
+    "spark.sql.catalog.postgres.uri": "jdbc:postgresql://postgres:5432/test",
+    "spark.sql.catalog.postgres.jdbc.user": "test",
+    "spark.sql.catalog.postgres.jdbc.password": "test",
+    "spark.sql.catalog.postgres.warehouse": "/home/iceberg/warehouse",
+    "spark.sql.defaultCatalog": "postgres",
+    "spark.eventLog.enabled": "true",
+    "spark.eventLog.dir": "/home/iceberg/spark-events",
+    "spark.history.fs.logDirectory": "/home/iceberg/spark-events",
+    "spark.sql.catalogImplementation": "in-memory",
+    "spark.sql.execution.arrow.pyspark.enabled": "true",
+}
+
 
 @pytest.fixture
 def io_manager(
@@ -17,6 +33,7 @@ def io_manager(
     return SparkIcebergIOManager(
         catalog_name=catalog_name,
         namespace=namespace,
+        spark_config=SPARK_CONFIG,
         remote_url="sc://localhost",
     )
 
@@ -37,7 +54,11 @@ def asset_b_plus_one_table_identifier(namespace: str) -> str:
     metadata={"partition_spec_update_mode": "update", "schema_update_mode": "update"},
 )
 def b_df() -> DataFrame:
-    spark = SparkSession.builder.remote("sc://localhost").getOrCreate()
+    spark = (
+        SparkSession.builder.remote("sc://localhost")
+        .config(map=SPARK_CONFIG)
+        .getOrCreate()
+    )
     return spark.createDataFrame(
         pa.Table.from_pydict({"a": [1, 2, 3], "b": [4, 5, 6]}).to_pandas()
     )
