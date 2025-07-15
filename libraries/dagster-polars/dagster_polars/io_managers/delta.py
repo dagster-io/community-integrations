cc895721109b9dcc7b10255ddd4285b1b78a4e83
leo <leonardobocci99@gmail.com>
2025-07-15 17:47:30 +0200

    ignore pyright for older syntax

--- libraries/dagster-polars/dagster_polars/io_managers/delta.py
+++ libraries/dagster-polars/dagster_polars/io_managers/delta.py
@@ -167,7 +167,8 @@ class PolarsDeltaIOManager(BasePolarsUPathIOManager):
                 )  # type: ignore
             except ValueError:
                 # ValueError: Invalid engine argument engine='streaming'
-                return self.write_df_to_path(context, df.collect(streaming=True), path)
+                # allow old syntax if installed polars version is older
+                return self.write_df_to_path(context, df.collect(streaming=True), path) # type: ignore
 
         else:
             return self.write_df_to_path(context, df.collect(), path)
