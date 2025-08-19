6dfe12abe26c48a486f6468f26abe7ba82ffe7e5
2705815 <Justin.Cohen2@aetna.com>
2025-08-19 16:40:08 -0400

    [dagster-dataform] import module objects into __init__.py

--- libraries/dagster-dataform/dagster_dataform/__init__.py
+++ libraries/dagster-dataform/dagster_dataform/__init__.py
@@ -1,4 +1,15 @@
 from dagster._core.libraries import DagsterLibraryRegistry
+from dagster_dataform.resources import (
+    DagsterDataformResource as DagsterDataformResource,
+    load_assets_from_dataform as load_assets_from_dataform,
+    load_asset_checks_from_dataform as load_asset_checks_from_dataform,
+)
+from dagster_dataform.dataform_polling_sensor import (
+    create_dataform_polling_sensor as create_dataform_polling_sensor,
+)
+from dagster_dataform.dataform_orchestration_schedule import (
+    create_dataform_orchestration_schedule as create_dataform_orchestration_schedule,
+)
 
 __version__ = "0.0.1"
 
