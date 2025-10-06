be59e784c9dc36c1d79da3f6fb090dd199b8b3fe
Georg Heiler <georg.kf.heiler@gmail.com>
2025-10-06 21:14:11 +0200

    feat: add tests

--- libraries/dagster-ducklake/dagster_ducklake/__init__.py
+++ libraries/dagster-ducklake/dagster_ducklake/__init__.py
@@ -1,14 +1,26 @@
 from dagster._core.libraries import DagsterLibraryRegistry
 
-from dagster_ducklake import (
-    DuckDBFileResource as DuckDBFileResource,
-    PostgresConfig as PostgresConfig,
-    SqliteConfig as SqliteConfig,
-    DuckDBConfig as DuckDBConfig,
-    S3Config as S3Config,
-    DuckLakeLocalDirectory as DuckLakeLocalDirectory,
-    DuckLakeResource as DuckLakeResource,
+from dagster_ducklake.ducklake import (
+    DuckDBConfig,
+    DuckLakeLocalDirectory,
+    DuckLakeResource,
+    PostgresConfig,
+    S3Config,
+    SqliteConfig,
 )
+from dagster_ducklake.duckdb_file import DuckDBFileResource
+from dagster_ducklake.duckdb_protocol import DuckDBConnectionProvider
+
+__all__ = [
+    "DuckDBConfig",
+    "DuckLakeLocalDirectory",
+    "DuckLakeResource",
+    "PostgresConfig",
+    "S3Config",
+    "SqliteConfig",
+    "DuckDBFileResource",
+    "DuckDBConnectionProvider",
+]
 
 __version__ = "0.0.1"
 
