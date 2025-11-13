9996e857e96e23cb1e6124ef5c3cef18bf1b9364
Steven Ayers <steven@ayers.io>
2025-11-13 12:20:24 +0000

    [chores] uv workspace

--- libraries/dagster-sftp/dagster_sftp/__init__.py
+++ libraries/dagster-sftp/dagster_sftp/__init__.py
@@ -1,11 +1,16 @@
+import importlib.metadata
+
 from dagster._core.libraries import DagsterLibraryRegistry
 
 from dagster_sftp.resource import (
-    SFTPResource as SFTPResource,
     SFTPFileInfo as SFTPFileInfo,
     SFTPFileInfoConfig as SFTPFileInfoConfig,
+    SFTPResource as SFTPResource,
 )
 
-__version__ = "0.0.2"
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
 
 DagsterLibraryRegistry.register("dagster-sftp", __version__, is_dagster_package=False)
