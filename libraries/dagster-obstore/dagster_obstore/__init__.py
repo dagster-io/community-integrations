d1669cb4c4ae68c2c4ca22b4012ce34a4fae1927
Colton Padden <cmpadden@users.noreply.github.com>
2026-03-19 11:03:31 -0400

    chore: prevent registry warnings for community integrations (#270)

--- libraries/dagster-obstore/dagster_obstore/__init__.py
+++ libraries/dagster-obstore/dagster_obstore/__init__.py
@@ -1,6 +1,6 @@
 from dagster._core.libraries import DagsterLibraryRegistry
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 DagsterLibraryRegistry.register(
     "dagster-obstore", __version__, is_dagster_package=False
