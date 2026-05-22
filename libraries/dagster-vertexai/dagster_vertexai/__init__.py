f480f7028d245acce226c6b8de262e38094d4053
Colton Padden <colton@dagsterlabs.com>
2026-05-22 15:45:17 -0400

    Release dagster-vertexai 0.0.2

--- libraries/dagster-vertexai/dagster_vertexai/__init__.py
+++ libraries/dagster-vertexai/dagster_vertexai/__init__.py
@@ -2,7 +2,7 @@ from dagster._core.libraries import DagsterLibraryRegistry
 
 from dagster_vertexai.resource import VertexAIResource as VertexAIResource
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 DagsterLibraryRegistry.register(
     "dagster-vertexai", __version__, is_dagster_package=False
