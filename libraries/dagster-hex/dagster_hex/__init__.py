0c3cc5f09863c3f13962b9e1f86de688fc163bd4
Colton Padden <colton@dagsterlabs.com>
2026-05-22 15:44:12 -0400

    Release dagster-hex 0.1.10

--- libraries/dagster-hex/dagster_hex/__init__.py
+++ libraries/dagster-hex/dagster_hex/__init__.py
@@ -1,5 +1,5 @@
 from dagster._core.libraries import DagsterLibraryRegistry
 
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 
 DagsterLibraryRegistry.register("dagster-hex", __version__, is_dagster_package=False)
