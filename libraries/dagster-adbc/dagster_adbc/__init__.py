64c6507b265765b35ca931bc3da2cf9e24584323
Colton Padden <colton@dagsterlabs.com>
2026-05-22 15:43:38 -0400

    Release dagster-adbc 0.0.2

--- libraries/dagster-adbc/dagster_adbc/__init__.py
+++ libraries/dagster-adbc/dagster_adbc/__init__.py
@@ -3,6 +3,6 @@ from dagster._core.libraries import DagsterLibraryRegistry
 from dagster_adbc.resource import ADBCResource
 
 __all__ = ["ADBCResource"]
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 DagsterLibraryRegistry.register("dagster-adbc", __version__, is_dagster_package=False)
